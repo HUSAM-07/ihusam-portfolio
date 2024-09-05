a45762afe3d308ed825c6201d3d768b85c49c9a9
Mohammed Husamuddin <uni.husam@gmail.com>
2024-09-06 02:14:09 +0400

    updated CMS

--- src/app/blog/[id]/page.tsx
+++ src/app/blog/[id]/page.tsx
@@ -91,7 +91,7 @@ export default async function Blog({
       <div className="flex justify-between items-center mt-2 mb-8 text-sm max-w-[650px]">
         <Suspense fallback={<p className="h-5" />}>
           <p className="text-sm text-neutral-600 dark:text-neutral-400">
-            {formatDate(new Date(post.publishedAt))}
+            {post.publishedAt ? formatDate(new Date(post.publishedAt)) : ''}
           </p>
         </Suspense>
       </div>
