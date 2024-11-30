f4877ba1c88f14875ba8ce11cca04ec365ee1c70
Mohammed Husamuddin <uni.husam@gmail.com>
2024-12-01 03:37:39 +0400

    Update icons.tsx

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -25,11 +25,15 @@ export const Icons = {
   ),
   newsletter: (props: IconProps) => (
     <svg
-      width="32px"
-      height="32px"
-      viewBox="0 0 32 32"
-      fill="currentColor"
       xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
       {...props}
     >
       <title>Newsletter</title>
